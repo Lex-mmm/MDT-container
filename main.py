dcc24bcff8318495b0851974caa47af505872c33
Ruben Zoodsma <r.s.zoodsma-2@umcutrecht.nl>
2025-04-28 10:43:24 +0200

    try

--- main.py
+++ main.py
@@ -128,7 +128,7 @@ def get_latest_data():
 
 if __name__ == '__main__':
     print("Starting server")
-    socketio.run(app, host="0.0.0.0", port=5001)
+    app.run(debug=True, host='127.0.0.1', port=5001)
 
     
 
